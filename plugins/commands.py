b0f9abe844762b644d2b275699a2c89c58bba529
saksham680 <86889757+saksham680@users.noreply.github.com>
2021-09-17 07:17:05 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,7 +1,7 @@
 from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-START_MSG = """**Hi {}
+START_MSG = """**Hi {please join my chhanel to use me @multiplex_movies}
   
 I am an Media Editor bot ...
 
@@ -9,7 +9,7 @@ You can edit document,video,gif,audio,photo etc..
 
 For More hit /help **
 
-"""
+"Please send"
 
 
 HELP_MSG = """
@@ -33,7 +33,7 @@ async def start(client, message):
     await message.reply_text(
         text=START_MSG.format(message.from_user.mention),
         disable_web_page_preview=True,
-        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="OWNER",url = "t.me/jack_of_tg")]]),
+        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="OWNER",url = "t.me/saksham_096")]]),
         reply_to_message_id=message.message_id
     )    
 
